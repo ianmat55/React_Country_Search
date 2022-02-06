7e4be754b84d793476dd9f387ba291f54208d746
ianmat55 <ianmat55@gmail.com>
2022-02-06 11:00:30 -0800

    styling edits

--- src/components/display.js
+++ src/components/display.js
@@ -5,8 +5,8 @@ function Weather({ weather }) {
 		const temp = (weather.main.temp - 273.15) * (9/5) + 32 // (275K − 273.15) × 9/5 + 32 
 		const weatherImg = `http://openweathermap.org/img/wn/${weather.weather[0]['icon']}.png`
 		return (
-			<div>
-				<h1> Weather in {weather.name} </h1>
+			<div id='weather' className='space'>
+				<h2> Weather in {weather.name} </h2>
 				<h3> Temperature: {temp.toFixed(2)} F </h3>
 				<div id='conditions'>
 					<h3> {weather.weather[0]['description']} </h3>
@@ -28,18 +28,20 @@ function Display({ country, weather }) {
 	if (country !== '' && country !== undefined) { // && != undefined fixes issue of deleting inputs and adding spaces, but theyre must be a better way
 		return (
 			<div id='displayWrapper'>
-				<div id='display'>
+				<div id='display' className='space'>
 					<img id='flag' src={display.flags.svg} alt={display.name.common} />
-					<h1 id='country-name'> {display.name.common} </h1>
-					<h3 id='capital'> Capital: {display.capital} </h3>
-					<h3 id='population'> Population: {display.population} </h3>
-					<div id='languages'>
-						<h3> Languages: </h3>
-						<ul>
-							{Object.keys(display.languages).map(index => {
-								return <li key={index}> {display.languages[index]} </li>
-							})}
-						</ul>
+					<div id='country-info' className='space'>
+						<h1 id='country-name'> {display.name.common} </h1>
+						<h3 id='capital'> Capital: {display.capital} </h3>
+						<h3 id='population'> Population: {display.population} </h3>
+						<div id='languages' className='space'>
+							<h3> Languages: </h3>
+							<ul>
+								{Object.keys(display.languages).map(index => {
+									return <li key={index}> {display.languages[index]} </li>
+								})}
+							</ul>
+						</div>
 					</div>
 					<Weather weather={weather} />
 				</div>
